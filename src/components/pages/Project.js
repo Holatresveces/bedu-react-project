3d8b21605bc2ead14e422ed88d7eb8562138ecb1
Lalo <eduardo.j.a@ciencias.unam.mx>
2022-05-17 19:29:19 -0500

    feat: adjust Project page content loading

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -13,8 +13,46 @@ const Test = ({ onContentLoaded }) => {
   });
 
   useEffect(() => {
-    onContentLoaded(null);
-  }, []);
+    if (!data) return;
+
+    const imgElements = [...document.querySelectorAll("img")];
+
+    const promises = imgElements.map((imgElement) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.addEventListener("load", () => {
+          resolve(imgElement);
+        });
+        img.src = imgElement.src;
+      });
+    });
+
+    Promise.all(promises).then((imgElements) => {
+      const data = imgElements.map((imgElement) => {
+        const src = imgElement.src;
+        const { left, top, width, height } = imgElement.getBoundingClientRect();
+
+        const bounds = {
+          left,
+          top: top + window.scrollY,
+          width,
+          height,
+          initX: left + width / 2 - window.innerWidth / 2,
+          initY: -top - height / 2 + window.innerHeight / 2,
+        };
+
+        console.log("bounds.initx", bounds.initX);
+
+        return {
+          domElement: imgElement,
+          src,
+          bounds,
+        };
+      });
+
+      onContentLoaded(data);
+    });
+  }, [data]);
 
   return (
     <>
